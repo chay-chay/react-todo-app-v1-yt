17b814535ce1c7cd6efef975e0ddc384deeb1942
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-06-09 15:11:12 -0400

    Edit function works

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -14,6 +14,19 @@ function TodoList() {
         console.log(...todos)
     }
 
+    const updateTodo = (todoId, newValue) => {
+        if (!newValue.text || /^s*$/.test(newValue.text)) {
+            return;
+        }
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)))
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+        setTodos(removeArr)
+    }
+
+
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
           if (todo.id === id) {
@@ -24,16 +37,12 @@ function TodoList() {
         setTodos(updatedTodos);
     };
     
-    const removeTodo = id => {
-        const removeArr = [...todos].filter(todo => todo.id !== id)
-        setTodos(removeArr)
-    }
-
+   
   return (
     <div>
       <h1>What's the plan for today?</h1>
           <TodoForm onSubmit={addTodo} />
-          <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+          <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
     </div>
   );
 }
